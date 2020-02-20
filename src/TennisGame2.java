6576f61c6bc56434b2862b53f0d26b3e567f6fbb
Juan5757 <52605634+Juan5757@users.noreply.github.com>
2020-02-20 00:36:40 -0400

    Variable score definido como variable global y eliminación de argumentos

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -8,26 +8,27 @@ public class TennisGame2 implements TennisGame
     public String P2res = "";
     private String player1Name;
     private String player2Name;
-
+    String score = "";
+    
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public String getScore(){
-        String score = "";
-        score = tie(score);
-        score = deuces(score);
         
-        score = normal(score);
+    	score = normal();
+    	
+        score = tie();
+        score = deuces();
+                 
+        score = advantage();
         
-        score = advantage(score);
-        
-        score = win(score);
+        score = win();
         return score;
     }
 
-	private String win(String score) {
+	private String win() {
 		if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
         {
             score = "Win for player1";
@@ -39,7 +40,7 @@ public class TennisGame2 implements TennisGame
 		return score;
 	}
 
-	private String advantage(String score) {
+	private String advantage() {
 		if (player1Points > player2Points && player2Points >= 3)
         {
             score = "Advantage player1";
@@ -52,7 +53,7 @@ public class TennisGame2 implements TennisGame
 		return score;
 	}
 
-	private String normal(String score) {
+	private String normal() {
 		if (player1Points != player2Points)
         {           
             score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
@@ -73,13 +74,13 @@ public class TennisGame2 implements TennisGame
 		return result;
 	}
 
-	private String deuces(String score) {
+	private String deuces() {
 		if (player1Points==player2Points && player1Points>=3)
             score = "Deuce";
 		return score;
 	}
 
-	private String tie(String score) {
+	private String tie() {
 		if (player1Points == player2Points && player1Points < 4)
         {
             if (player1Points==0)
