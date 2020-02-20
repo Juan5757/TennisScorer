b89d3f27d74948fc363beb6cc51c8e4e7c82852a
Juan5757 <52605634+Juan5757@users.noreply.github.com>
2020-02-20 00:24:14 -0400

    Cambio de nombre de variables a player1Points y player2Points

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -1,8 +1,8 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int P1point = 0;
-    public int P2point = 0;
+    public int player1Points = 0;
+    public int player2Points = 0;
     
     public String P1res = "";
     public String P2res = "";
@@ -32,11 +32,11 @@ public class TennisGame2 implements TennisGame
     }
 
 	private String win(String score) {
-		if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+		if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (player2Points>=4 && player1Points>=0 && (player2Points-player1Points)>=2)
         {
             score = "Win for player2";
         }
@@ -44,12 +44,12 @@ public class TennisGame2 implements TennisGame
 	}
 
 	private String advantage(String score) {
-		if (P1point > P2point && P2point >= 3)
+		if (player1Points > player2Points && player2Points >= 3)
         {
             score = "Advantage player1";
         }
         
-        if (P2point > P1point && P1point >= 3)
+        if (player2Points > player1Points && player1Points >= 3)
         {
             score = "Advantage player2";
         }
@@ -57,33 +57,33 @@ public class TennisGame2 implements TennisGame
 	}
 
 	private String normal3(String score) {
-		if (P2point != P1point)
+		if (player2Points != player1Points)
         {
-            score = getLiteral(P1point) + "-" + getLiteral(P2point);
+            score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
         }
 		return score;
 	}
 
 	private String normal2(String score) {
-		if (P1point != P2point)
+		if (player1Points != player2Points)
         {   
-            score = getLiteral(P1point) + "-" + getLiteral(P2point);
+            score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
         }
 		return score;
 	}
 
 	private String normal1(String score) {
-		if (P2point != P1point)
+		if (player2Points != player1Points)
         { 
-            score = getLiteral(P1point) + "-" + getLiteral(P2point);
+            score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
         }
 		return score;
 	}
 
 	private String normal(String score) {
-		if (P1point != P2point)
+		if (player1Points != player2Points)
         {           
-            score = getLiteral(P1point) + "-" + getLiteral(P2point);
+            score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
         }
 		return score;
 	}
@@ -102,19 +102,19 @@ public class TennisGame2 implements TennisGame
 	}
 
 	private String deuces(String score) {
-		if (P1point==P2point && P1point>=3)
+		if (player1Points==player2Points && player1Points>=3)
             score = "Deuce";
 		return score;
 	}
 
 	private String tie(String score) {
-		if (P1point == P2point && P1point < 4)
+		if (player1Points == player2Points && player1Points < 4)
         {
-            if (P1point==0)
+            if (player1Points==0)
                 score = "Love";
-            if (P1point==1)
+            if (player1Points==1)
                 score = "Fifteen";
-            if (P1point==2)
+            if (player1Points==2)
                 score = "Thirty";
             score += "-All";
         }
@@ -140,11 +140,11 @@ public class TennisGame2 implements TennisGame
     }
     
     public void P1Score(){
-        P1point++;
+        player1Points++;
     }
     
     public void P2Score(){
-        P2point++;
+        player2Points++;
     }
 
     public void wonPoint(String player) {
